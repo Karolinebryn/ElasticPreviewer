905f58341b9450f9cfe0d20135382d3d136f27b4
Karolinebryn <karolinebrynildsen@gmail.com>
2016-11-05 23:35:47 +0100

    adding elastic controller and viewmodels

---
+++ ElasticPreviewer/ElasticPreviewer/Controllers/ElasticController.cs
@@ -0,0 +1,19 @@
+﻿using ElasticPreviewer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElasticPreviewer.Controllers
+{
+    public class ElasticController : ApiController
+    {
+        public ElasticResultViewModel Post(ElasticSearchViewModel search)
+        {
+            //do shit
+            return new ElasticResultViewModel { Result = "some json" };
+        }
+    }
+}
---
+++ ElasticPreviewer/ElasticPreviewer/Models/ElasticResultViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElasticPreviewer.Models
+{
+    public class ElasticResultViewModel
+    {
+        public string Result { get; set; }
+    }
+}
